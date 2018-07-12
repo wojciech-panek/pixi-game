f0d1b1b50b8cc173d7788671c0b3b70b2c06c302
Michał Kubiak <mkubiak@apptension.com>
2018-07-12 05:05:45 +0200

    Create simple routing

--- webapp/app/main.js
+++ webapp/app/main.js
@@ -2,6 +2,18 @@ import 'babel-polyfill';
 import 'normalize.css/normalize.css';
 
 import { App } from './components/app';
+import { ControllerApp } from './controller/app';
 import { Loader } from './utils/loader';
 
 Loader.load().then(() => new App({ elementId: '#app' }));
+const { pathname } = window.location;
+
+if (pathname.includes('controller')) {
+  const app = new ControllerApp();
+  app.render('#app');
+} else {
+  Loader.load().then(() => {
+    const app = new App();
+    app.render('#app');
+  });
+}
