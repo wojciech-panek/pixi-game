7407d4ffba1d26c450ea0f162753a7394737cbfd
Michał Kubiak <mkubiak@apptension.com>
2018-07-12 14:33:27 +0200

    Remove unused paths

--- webapp/gameServer/server.js
+++ webapp/gameServer/server.js
@@ -10,9 +10,9 @@ const app = express();
 const server = new http.Server(app);
 const io = socketIO(this.server);
 const port = process.env.PORT ? process.env.PORT : 8181;
-const dist = path.join(__dirname, '../dist');
+// const dist = path.join(__dirname, '../dist');
 
-app.use(express.static(dist));
+// app.use(express.static(dist));
 // app.get('*', (req, res) => res.sendfile(path.join(__dirname, '../dist/index.html')));
 
 server.listen(this.port, () => {
